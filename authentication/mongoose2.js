2842f39c4749e8564c074d3304e0721e283efec3
neels22 <indraneelsarode@gmail.com>
2024-09-15 20:28:38 +0530

    removed url

--- authentication/mongoose2.js
+++ authentication/mongoose2.js
@@ -5,7 +5,6 @@ const mongoose = require("mongoose")
 const app = express()
 app.use(express.json())
 
-mongoose.connect("mongodb+srv://new:zoC0r9hs0WkQIPQ8@cluster0.9yali.mongodb.net/user_app")
 const User = mongoose.model('users', {name:String ,email:String,password:String} )
 
 
