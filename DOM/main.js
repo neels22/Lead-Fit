cbaef771f7eb9d9c3930089b3d1c4cc03be02c3f
neels22 <indraneelsarode@gmail.com>
2024-09-19 17:57:16 +0530

    sending request to backend servers

--- DOM/main.js
+++ DOM/main.js
@@ -1,2 +1,19 @@
 
 
+
+const express= require("express")
+
+const app = express()
+
+app.get("/sum",function (req,res)   {
+
+    const num1 = parseInt(req.query.a) 
+    const num2 = parseInt(req.query.b)
+
+    const sum = num1+num2 
+
+    res.send(sum.toString())
+    
+})
+
+app.listen(3000)
