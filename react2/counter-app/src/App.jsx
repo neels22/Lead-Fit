af78a957918ea5028b0e6dbf6d0e0fa6776f746f
neels22 <indraneelsarode@gmail.com>
2024-09-24 12:49:47 +0530

    basic todo app using react

--- react2/counter-app/src/App.jsx
+++ react2/counter-app/src/App.jsx
@@ -11,27 +11,77 @@ import { useState } from "react"
 function App() {
 
   // defining the initial state 
-  const [count,setCount] = useState(0) // [0,1]
+  // const [count,setCount] = useState(0) // [0,1]
+
+  const [todo,setTodo] =  useState([{
+
+    title:"go to gym",
+    desc:"from 7 to 9"
+
+
+  },{
+    title:"study dsa",
+    desc:"from 9 to 11"
+  }])
+
+
+
+  
+  function addTodo() {
+    setTodo([...todo,{
+      title:"new todo",
+      desc:"this is added from button"
+    }])
+  }
+
 
   return (
     <div>
-      <CustomButton count={count} setCount ={setCount} ></CustomButton>
+      {/* <CustomButton count={count} setCount ={setCount} ></CustomButton> */}
+
+    <button onClick={addTodo}>add new todo</button>
+
+    {/* <Todo title={todo[0].title} desc={todo[0].desc}/>
+    <Todo title={todo[1].title} desc={todo[1].desc}/> */}
+
+      {/* better approach is to use map  */}
+
+      {
+        todo.map(function (todo) {
+          return <Todo title={todo.title} desc={todo.desc}/>
+        })
+      }
+
+
+
     </div>
   )
 }
 
 // creating button component separate 
  
-function CustomButton(props) {
+// function CustomButton(props) {
 
-  function onclickhandler() {
-    // right way to set state variables 
-    props.setCount(props.count +1)
+//   function onclickhandler() {
+//     // right way to set state variables 
+//     props.setCount(props.count +1)
 
-  }
-  return <button onClick={onclickhandler}>
-    counter {props.count}
-  </button>
+//   }
+//   return <button onClick={onclickhandler}>
+//     counter {props.count}
+//   </button>
+  
+// }
+
+//// creating Todo component 
+function Todo(props) {
+
+  return <div>
+
+    <h1>{props.title}</h1>
+    <h2>{props.desc}</h2>
+
+  </div>
   
 }
 
