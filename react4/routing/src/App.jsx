133ee5c483feb71c99ede1df0ec5842d1928d575
neels22 <indraneelsarode@gmail.com>
2024-10-03 12:27:47 +0530

    suspense api

--- react4/routing/src/App.jsx
+++ react4/routing/src/App.jsx
@@ -1,55 +1,37 @@
+import React, { Suspense } from 'react'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 
-
-
-
-import { Dashboard } from './components/Dashboard'
-import { Landing } from './components/Landing'
-import { BrowserRouter, Route, Routes,useNavigate } from 'react-router-dom'
+const Dashboard = React.lazy(() => import("./components/Dashboard"))
+const Landing = React.lazy(() => import("./components/Landing"))
 
 function App() {
-
-
   return (
-
-<div>
-  
-    <BrowserRouter>
-        
+    <div>
+      <BrowserRouter>
         <Appbar />
-
-  <Routes>
-    <Route path='/dashboard' element={<Dashboard/>}/>
-    <Route path='/landing' element={<Landing/>} />
-    </Routes>
-    </BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/landing' element={<Landing />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
     </div>
   )
 }
 
 function Appbar() {
-
   const navigate = useNavigate()
 
-  return(
-
+  return (
     <div>
-      <div >
+      <div>
         hi this is from the div
-        <button onClick={()=>{
-          navigate("/landing")
-        }}>landing page</button>
-        <button onClick={()=>{
-          
-          navigate("/dashboard")
-
-        }} >dashboard</button>
+        <button onClick={() => navigate("/landing")}>Landing page</button>
+        <button onClick={() => navigate("/dashboard")}>Dashboard</button>
       </div>
       <br />
-      </div>
-      
-
-
-
+    </div>
   )
 }
 
