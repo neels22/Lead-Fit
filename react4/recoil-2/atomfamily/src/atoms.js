e50258f0a16aa61f4efb5e79834d87e362d36bf9
neels22 <indraneelsarode@gmail.com>
2024-10-07 12:50:06 +0530

    atoms family in recoil

---
+++ react4/recoil-2/atomfamily/src/atoms.js
@@ -0,0 +1,13 @@
+import { atomFamily } from 'recoil';
+
+export const TODOS = [
+  { id: 1, title: 'Go to Gym', description: 'Hit the gym from 7-9' },
+  { id: 2, title: 'Go to eat', description: 'Eat' },
+];
+
+export const todosAtomFamily = atomFamily({
+  key: 'todosAtomFamily',
+  default: (id) => {
+    return TODOS.find((x) => x.id === id);
+  },
+});
